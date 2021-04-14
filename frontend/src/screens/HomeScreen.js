b5a2aa53fe82bc7f85ac2f06a17f97088cb6586a
jztimm <jztimm@gmail.com>
2021-04-14 12:47:43 -0400

    Created screens folder and homescreen for new products

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import products from '../products'
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Newest Products</h1>
+      <Row>
+
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
