b4f499219c6163fe5c0b527979b1cbb665e07d73
jztimm <jztimm@gmail.com>
2021-04-14 12:38:19 -0400

    Updated Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Navbar, Nav, Container} from 'react-bootstrap'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const Header = () => {
   return (
     <header>
-      header
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Merch Store</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart"><ShoppingCartIcon/> Cart</Nav.Link>
+              <Nav.Link href="/login"><AccountCircleIcon/> Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+    </Navbar>
     </header>
   )
 }
