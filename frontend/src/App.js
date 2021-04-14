adb39b0ba95bd4ef57c1876ff8b35680d8194cd7
jztimm <jztimm@gmail.com>
2021-04-14 12:42:09 -0400

    Added some spacing above title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>Welcome to General Merch</h1>
         </Container>
