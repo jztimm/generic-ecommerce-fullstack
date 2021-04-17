7fa1d1dc25a1ce7bffe4220b426ad8cdaeaaa9e8
jztimm <jztimm@gmail.com>
2021-04-17 15:47:15 -0400

    updated color of the rating stars

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -7,25 +7,30 @@ const Rating = ({ value, text, color }) => {
   return (
     <div className='rating'>
       <span>
-      { value >= 1 ? <StarIcon style={{ color: 'yellow' }}/> : value >= 0.5 ? <StarHalfIcon style={{ color: 'yellow' }}/> : <StarBorderIcon/>}
+      { value >= 1 ? <StarIcon style={{ color }}/> : value >= 0.5 ? <StarHalfIcon style={{ color }}/> : <StarBorderIcon style={{ color }}/>}
       </span>
       <span>
-        {value >= 2 ? <StarIcon style={{ color: 'yellow' }}/> : value >= 1.5 ? <StarHalfIcon style={{ color: 'yellow' }}/> : <StarBorderIcon/>}
+        {value >= 2 ? <StarIcon style={{ color }}/> : value >= 1.5 ? <StarHalfIcon style={{ color }}/> : <StarBorderIcon style={{ color }}/>}
       </span>
       <span>
-        {value >= 3 ? <StarIcon style={{ color: 'yellow' }}/> : value >= 2.5 ? <StarHalfIcon style={{ color: 'yellow' }}/> : <StarBorderIcon/>}
+        {value >= 3 ? <StarIcon style={{ color }}/> : value >= 2.5 ? <StarHalfIcon style={{ color }}/> : <StarBorderIcon style={{ color }}/>}
       </span>
       <span>
-        {value >= 4 ? <StarIcon style={{ color: 'yellow' }}/> : value >= 3.5 ? <StarHalfIcon style={{ color: 'yellow' }}/> : <StarBorderIcon/>}
+        {value >= 4 ? <StarIcon style={{ color }}/> : value >= 3.5 ? <StarHalfIcon style={{ color }}/> : <StarBorderIcon style={{ color }}/>}
       </span>
       <span>
-        {value >= 5 ? <StarIcon style={{ color: 'yellow' }}/> : value >= 4.5 ? <StarHalfIcon style={{ color: 'yellow' }}/> : <StarBorderIcon/>}
+        {value >= 5 ? <StarIcon style={{ color }}/> : value >= 4.5 ? <StarHalfIcon style={{ color }}/> : <StarBorderIcon style={{ color }}/>}
       </span>
 
       {/* Better than writing  text ? text : '' */}
       <span>{text && text}</span>
     </div>
   )
+}
+
+// The custome stlye below gives the default value color to the hex of yellow
+Rating.defaultProps = {
+  color: '#f8e825'
 }
 
 export default Rating
