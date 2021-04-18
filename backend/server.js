d62a3c0765598a3aa6e2773c597d7c5fea99a160
jztimm <jztimm@gmail.com>
2021-04-18 17:27:41 -0400

    Fixed backend err ln 20, arr not defined in find

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get('/api/products', (req, res) => {
 })
 
 app.get('/api/products/:id', (req, res) => {
-  const product = find(p => p._id === req.params.id)
+  const product = products.find(p => p._id === req.params.id)
   res.json(product)
 })
 
