1566efaaff36fff5826275798bde7ffcab0a1339
jztimm <jztimm@gmail.com>
2021-04-26 11:56:33 -0400

    Added .js to imported file line 5 in productRoutes.js. Setup fetch calls to DB

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,18 +2,31 @@ import express from 'express'
 import asyncHandler from 'express-async-handler'
 
 const router = express.Router()
-import Product from '../models/productModel'
+import Product from '../models/productModel.js'
 
-router.get('/', asyncHandler (async(req, res) => {
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+router.get('/', asyncHandler (async(req, res) => {        // Using express-async-handler below to handle the returned promise
+  // Below products will return a promise and rather than returning that promise, use await instead
   const products = await Product.find({})
   
   // having .send or .json will both send info in it's correct format
   res.json(products)
 }))
 
-router.get('/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+// @desc Fetch single products
+// @route GET /api/products/:id
+// @access Public
+router.get('/:id', asyncHandler (async(req, res) => {
+  const product = await Product.findById(req.params.id)   // Will give us ID that is in the URL
+
+  if(product) {
+    res.json(product)                                     // If product is true, return the product from JSON
+  } else {
+    res.status(404).json({ message: 'Product not found' })  // 404 error message for product not found
+  }
+
+}))
 
 export default router
