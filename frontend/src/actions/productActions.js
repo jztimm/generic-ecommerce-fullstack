4a162fd509a8775221dc32d704186621856f137a
jztimm <jztimm@gmail.com>
2021-04-27 11:21:51 -0400

    Create redux actions, reducers and dispatch

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL
+} from '../constants/productConstants'
+
+export const listProducts = () => async (dispatch) => {     // fetch data from api and dispatch actions to the reducer
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST })    // Call in reducer and set loading to true, prod will be empty
+
+    const {data} = await axios.get('/api/products')
+
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_ERROR,
+      // If we have a custome error msg, use that. If not then use the generic error msg
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    })
+  }
+}
