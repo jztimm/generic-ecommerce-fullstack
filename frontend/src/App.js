759979c5bf145aea059f8d05d1cd28319aaa9271
jztimm <jztimm@gmail.com>
2021-04-28 14:18:13 -0400

    Created quantity Selector and updated add to cart button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Container>
           <Route path="/" component={HomeScreen} exact/>          {/* to view this screen, must have exact path of "/" */}
           <Route path="/product/:id" component={ProductScreen}/>  {/* /:id is a placeholder for anything after product path */}
+          <Route path="/cart/:id?" component={CartScreen}/>  {/* /cart is the path and /:id? is a optional */}
         </Container>
       </main>
       <Footer />
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem, Form} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { listProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
+  const [qty, setQty] = useState(0)
+
+
   const dispatch = useDispatch()
 
   const productDetails = useSelector(state => state.productDetails)
@@ -17,16 +20,23 @@ const ProductScreen = ({ match }) => {
     dispatch(listProductDetails(match.params.id))
   }, [dispatch, match])
 
+  // Add to cart button handler
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
+
   return (
     <>
       <Link className='btn btn-secondary my-3' to='/'>
         Go Back
       </Link>
+
       {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
       <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid/> {/* fluid keeps in it's container */}
         </Col>
+
         <Col md={3}>
           <ListGroup variant='flush'>   {/* Removes the boarder */}
             <ListGroup.Item>
@@ -43,6 +53,7 @@ const ProductScreen = ({ match }) => {
             </ListGroup.Item>
           </ListGroup>
         </Col>
+
         <Col md={3}>
           <Card>
             <ListGroup variant='flush'>
@@ -54,6 +65,7 @@ const ProductScreen = ({ match }) => {
                 </Col>
                 </Row>
               </ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Status:</Col>
@@ -62,8 +74,31 @@ const ProductScreen = ({ match }) => {
                 </Col>
                 </Row>
               </ListGroup.Item>
+
+              {product.inStockCounter > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Qty</Col>
+                    <Col>
+                      <Form.Control as='select' value={qty} onChange={(e) => setQty(e.target.value)}>
+                        {[...Array(product.inStockCounter).keys()].map(x => (
+                          <option key={x+1} value={x+1}>
+                            {x+1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
+
               <ListGroup.Item>
-                <Button className="btn-block btn-secondary" type="button" disabled={product.inStockCounter === 0}>
+                <Button
+                  onClick={addToCartHandler}
+                  className="btn-block btn-secondary"
+                  type="button"
+                  disabled={product.inStockCounter === 0}
+                >
                   Add To Cart
                 </Button>
               </ListGroup.Item>
