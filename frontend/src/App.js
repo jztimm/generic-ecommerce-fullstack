11047930b182b8240028d8699e7acf09b9231172
jztimm <jztimm@gmail.com>
2021-04-29 22:39:44 -0400

    Created user register, reducer, action, and screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import loginScreen from './screens/loginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path="/login" component={loginScreen}/>  {/* Redirects you to the login page*/}
+          <Route path="/register" component={RegisterScreen}/>  {/* Redirects you to the login page*/}
           <Route path="/product/:id" component={ProductScreen}/>  {/* /:id is a placeholder for anything after product path */}
           <Route path="/cart/:id?" component={CartScreen}/>  {/* /cart is the path and /:id? is a optional */}
           <Route path="/" component={HomeScreen} exact/>          {/* to view this screen, must have exact path of "/" */}
