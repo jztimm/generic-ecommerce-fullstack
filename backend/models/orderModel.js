7201467f9330ade8c9fc476c04b895cb37faeaf6
jztimm <jztimm@gmail.com>
2021-05-02 12:21:26 -0400

    Updated orderModel in backend/models

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -11,7 +11,7 @@ const orderSchema = mongoose.Schema({
   orderItems: [
     {
       name: { type: String, required: true },
-      quantity: { type: Number, required: true },
+      qty: { type: Number, required: true },
       image: { type: String, required: true },
       price: { type: Number, required: true },
       // refs product from productSchema
@@ -24,13 +24,13 @@ const orderSchema = mongoose.Schema({
   ],
   // Order shipping information
   shippingAddress: {
-    adress: { type: String, required: true },
+    address: { type: String, required: true },
     city: { type: String, required: true },
     postalCode: { type: String, required: true },
     country: { type: String, required: true },
   },
   // Order payment information
-  paymentMethods: {
+  paymentMethod: {
     type: String,
     required: true,
   },
